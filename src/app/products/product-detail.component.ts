36851994a71017dbdd8ba648af18c3cdde69bebe
sergethi <nsergearistide@gmail.com>
2022-03-14 17:06:46 +0000

    added product detail route and refractored code modules and shared files

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
   // selector: 'pm-product-detail',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
-  constructor() { }
+  product: IProduct | undefined;
+
+  constructor( private route:ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pageTitle += `: ${id}`;
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products'])
   }
 
 }
